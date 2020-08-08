1b383b44343a0d1a6cf9a3e46e685513938e1644
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-08 21:47:19 +0900

    deviseストロングパラメーター(追加)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_kana, :first_kana, :birthday])
   end
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
