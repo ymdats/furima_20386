6e06bdb190a396cb09c1fb3cc7e3e8d4321a65ba
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-08 20:27:01 +0900

    入力データにvalidation適用

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/, message: 'メールアドレスを入れてください'}
+    validates :password, format: { with: /[a-z\d]{6,}/, message: '6文字以上の半角英数字混合で入力してください'}
+    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）で入力してください' }
+    validates :family_kana, :first_kana, format: { with: /\A[ァ-ン]+\z/, message: '全角（カタカナ）で入力してください' }
+    validates :birthday
+  end
 end
