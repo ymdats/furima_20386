ff6dcad2558ab0d777b3791bc475383f62b151de
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-09 20:10:44 +0900

    とりあえずitemsコントローラーのベース作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,31 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+  
   def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
