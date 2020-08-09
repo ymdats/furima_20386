e33a3a40ac419bcada9a95fdffa590a7aa67986a
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-09 21:31:20 +0900

    FactoryBot各インスタンス値作成

---
+++ spec/factories/user.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    gimei = Gimei.name
+    family_name           {gimei.last.kanji}
+    first_name            {gimei.first.kanji}
+    family_kana           {gimei.last.katakana}
+    first_kana            {gimei.first.katakana}
+    birthday              {Faker::Date.birthday}
+  end
+end
