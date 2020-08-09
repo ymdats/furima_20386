3e736572a5e5ce77d3c21ef6a64d80fa7c3c3f0a
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-09 21:52:36 +0900

    Revert "パスワード6字以上制限入れ忘れ修正"
    This reverts commit 5bfa5de280ed18c7aece69b73d4c93902c704815.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/, message: 'メールアドレスを入れてください'}
-    validates :password, length: { minimum: 6 }, format: { with: /[a-z\d]{6,}/, message: '6文字以上の半角英数字混合で入力してください'}
+    validates :password, format: { with: /[a-z\d]{6,}/, message: '6文字以上の半角英数字混合で入力してください'}
     validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）で入力してください' }
     validates :family_kana, :first_kana, format: { with: /\A[ァ-ン]+\z/, message: '全角（カタカナ）で入力してください' }
     validates :birthday
