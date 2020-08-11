a7c57dd5c85cd29ce35513268226ef2f2acd0a24
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-11 18:54:38 +0900

    FactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Lorem.word}
+    explain {Faker::Lorem.sentence}
+    category_id {Faker::Number.between(from: 2, to: 11)}
+    status_id {Faker::Number.between(from: 2, to: 11)}
+    shipping_charge_id {Faker::Number.between(from: 2, to: 11)}
+    shipping_area_id {Faker::Number.between(from: 2, to: 11)}
+    shipping_duration_id {Faker::Number.between(from: 2, to: 11)}
+    price {Faker::Number.between(from: 300, to: 9999999)}
+    association :user 
   end
 end
