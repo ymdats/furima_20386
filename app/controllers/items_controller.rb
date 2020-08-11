1539982851a323f6fc2229e63955f038ebd5a50d
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-11 13:22:20 +0900

    画像アップロード機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,7 @@ class ItemsController < ApplicationController
   end
 
   def create
+
   end
 
   def destroy
@@ -26,5 +27,8 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+  def item_params
+    params.require(:item).permit(:image, :name, :explain, :category, :status, :shipping_charge, :shipping_area, :shipping_duration, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   with_options presence: true do
     validates :image, :name, :explain, :category, :status, :shipping_charge, :shipping_area, :shipping_duration, :price
   end
