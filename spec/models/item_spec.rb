51fda1f3af8196c3e808167b5c7f0d26d27584ff
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-11 18:54:51 +0900

    単体テストコード作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('public/images/simpson.png')
+  end
+
+  describe '商品の保存' do
+    context "商品が保存できる場合" do
+      it "全ての商品カラムが存在すれば保存される" do
+        expect(@item).to be_valid
+      end
+      it "テキストのみあればツイートは保存される" do
+      end
+    end
+    context "商品が保存できない場合" do
+      it "商品名が無いと保存できない" do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "商品説明が無いと保存できない" do
+        @item.explain = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explain can't be blank")
+      end
+      it "カテゴリーが選択されないと保存できない" do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it "状態が選択されないと保存できない" do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
+      end
+      it "配送料負担が選択されないと保存できない" do
+        @item.shipping_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge must be other than 1")
+      end
+      it "発送元地域が選択されないと保存できない" do
+        @item.shipping_area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
+      end
+      it "発送日数が選択されないと保存できない" do
+        @item.shipping_duration_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping duration must be other than 1")
+      end
+      it "価格が300円未満だと保存できない" do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "価格が10,000,000円以上だと保存できない" do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+  end
 end
