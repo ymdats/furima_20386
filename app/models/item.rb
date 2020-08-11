93c92f6901c090621372c54bff97d2a14d19f9eb
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-11 16:14:55 +0900

    金額上限加減を追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_duration
 
   with_options presence: true do
-    validates :image, :name, :explain, :price
+    validates :image, :name, :explain
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     with_options numericality: { other_than: 1 } do
       validates :category_id, :status_id, :shipping_charge_id, :shipping_area_id, :shipping_duration_id
     end
