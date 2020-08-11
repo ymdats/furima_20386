0ccf646b4a7d3d9d8475d772526b0d0976ee7eb5
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-11 14:03:44 +0900

    カテゴリ選択の導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associatons::ActiveRecordExtemsions
+  belongs_to_active_hash :category, :status, :shipping_charge, :shipping_area, :shipping_duration
+
   with_options presence: true do
-    validates :image, :name, :explain, :category, :status, :shipping_charge, :shipping_area, :shipping_duration, :price
+    validates :image, :name, :explain, :price
+    with_options numericality: { other_than: 1 } do
+      validates :category, :status, :shipping_charge, :shipping_area, :shipping_duration
+    end
   end
 end
---
+++ spec/factories/shipping_durations.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :shipping_duration do
+    
+  end
+end
