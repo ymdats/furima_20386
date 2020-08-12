9775afc4883cc0177097fd24c225efaf1c00cd67
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-12 20:48:17 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     if @item.update(item_params)
       @item.update(item_params)
-      redirect_to item_path(item.id)
+      redirect_to item_path(@item.id)
     else
       # redirect_to edit_item_path(item.id)
       render :edit
