2e44d957b7845e6942ecb49efcb0de60494b599f
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-15 01:43:46 +0900

    住所モデルのアソシエーションとバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,10 @@
 class Address < ApplicationRecord
+  belongs_to :buy
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  with_options presence: true do
+    validates :postal_code, :city, :house_number, :phone_number
+    validates :prefecture, numericality: { other_than: 1 }
+  end
 end
