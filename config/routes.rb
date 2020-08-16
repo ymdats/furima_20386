5a7dfb0a61b24a07ecf3c58fd5e2ca6f41a586c7
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-16 22:56:20 +0900

    ルーティング修正(ネスト構造)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  resources :items
-  resources :addresses, only:[:show, :create]
+  resources :items do
+    resources :buys, only: [:index, :create]
+  end
 end
