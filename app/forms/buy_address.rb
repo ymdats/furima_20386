821e255bb738c108aad0e8e6675bd977e8c1e548
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-17 03:16:47 +0900

    Validation記載ミス修正

--- app/forms/buy_address.rb
+++ app/forms/buy_address.rb
@@ -4,8 +4,8 @@ class BuyAddress
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :postal_code, format: {with: /\A[0-9]{11}\z/, message: "is invalid. Not include hyphen(-)"}
-    validates :postal_code, :city, :house_number
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "is invalid. Not include hyphen(-)"}
+    validates :city, :house_number
     validates :prefecture_id, numericality: { other_than: 1 }
   end
 
