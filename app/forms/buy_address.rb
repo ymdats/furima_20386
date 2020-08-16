15972d437f99bcaabae0cda0537f683714076e06
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-17 00:51:46 +0900

    郵便番号と電話番号の許可条件

--- app/forms/buy_address.rb
+++ app/forms/buy_address.rb
@@ -3,7 +3,9 @@ class BuyAddress
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :buy_id, :token
 
   with_options presence: true do
-    validates :postal_code, :city, :house_number, :phone_number
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{11}\z/, message: "is invalid. Not include hyphen(-)"}
+    validates :postal_code, :city, :house_number
     validates :prefecture_id, numericality: { other_than: 1 }
   end
 
