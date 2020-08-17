9e75e746ee4710bdb963fe11fb2174f348078bd0
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-17 21:33:58 +0900

    正規変数を変数に代入

--- app/forms/buy_address.rb
+++ app/forms/buy_address.rb
@@ -2,9 +2,12 @@ class BuyAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :buy_id, :token
 
+  postal = /\A[0-9]{3}-[0-9]{4}\z/
+  phone = /\A[0-9]{11}\z/
+
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid. Not include hyphen(-)' }
+    validates :postal_code, format: { with: postal, message: 'is invalid. Include hyphen(-)' }
+    validates :phone_number, format: { with: phone, message: 'is invalid. Not include hyphen(-)' }
     validates :city, :house_number
     validates :prefecture_id, numericality: { other_than: 1 }
   end
