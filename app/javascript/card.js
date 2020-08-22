8b965624190ab6780a15c7d8c587f37dcf649735
ymdats <61939802+ymdats@users.noreply.github.com>
2020-08-22 20:37:36 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
